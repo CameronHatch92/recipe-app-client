b423a77a2e414ebc4253411d9d2e4de8b459f52c
Cameron Hatch <cameronlavigne92@gmail.com>
2018-11-21 12:34:25 -0700

    added headers for requests

--- src/index.js
+++ src/index.js
@@ -7,13 +7,24 @@ import {ApolloProvider} from 'react-apollo';
 import {createHttpLink} from 'apollo-link-http';
 import {ApolloClient} from 'apollo-client';
 import {InMemoryCache} from 'apollo-cache-inmemory';
+import {setContext} from 'apollo-link-context';
 
 const serverUrl = createHttpLink({
   uri: 'http://localhost:4000/graphql'
 });
 
+const authLink = setContext((_, {headers}) => {
+  const token = localStorage.getItem('token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    }
+  }
+});
+
 const client = new ApolloClient({
-  link: serverUrl,
+  link: authLink.concat(serverUrl),
   cache: new InMemoryCache()
 })
 
