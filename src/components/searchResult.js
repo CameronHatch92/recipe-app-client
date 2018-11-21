eaeb9ae67e866d9845bf84b65ecea74cf3becf2c
Cameron Hatch <cameronlavigne92@gmail.com>
2018-11-21 16:00:32 -0700

    working on adding recipe

--- src/components/searchResult.js
+++ src/components/searchResult.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './recipe.css';
 import {Mutation} from 'react-apollo';
 import {ADD_RECIPE} from '../Mutations';
+import { GET_RECIPES } from '../Queries';
 
 class SearchResult extends Component{
 
@@ -9,7 +10,7 @@ class SearchResult extends Component{
 
 
   render(){
-
+    const {title, image_url, source_url, publisher, publisher_url} = this.recipe;
     return(
       <div className='recipe-container'>
         <h3 className='recipe-title'>{this.recipe.title}</h3>
@@ -18,7 +19,23 @@ class SearchResult extends Component{
           <div className='recipe-info'>
             <a href={this.recipe.source_url} target={'blank'}>Get the recipe here!</a>
             <p>More from <a href={this.recipe.publisher_url} target={'blank'}>{this.recipe.publisher}</a></p>
-            <button>Save</button>
+            <Mutation mutation={ADD_RECIPE} variables={{title, image_url, source_url, publisher, publisher_url}}
+            update={(store, {data}) => {
+              const currentStoreState = store.readQuery({query: GET_RECIPES});
+              let newStoreState;
+              console.log(data.addRecipe);
+              if(currentStoreState.recipes){
+                newStoreState = [...currentStoreState.recipes, data.addRecipe];
+              }else{
+                newStoreState = [data.addRecipe];
+              }
+              store.writeQuery({
+                query: GET_RECIPES,
+                data: {recipes: newStoreState}
+              })
+            }}>
+              {parsedLink => <button onClick={parsedLink}>Save Recipe!</button>}
+            </Mutation>
           </div>
         </div>
       </div>
